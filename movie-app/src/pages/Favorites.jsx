aec8599ea4fc42778f0066b55ba8299e7fbde514
Prathu <prathamesh4402@gmail.com>
2025-04-05 23:12:36 -0600

    movie app setup

---
+++ movie-app/src/pages/Favorites.jsx
@@ -0,0 +1,8 @@
+function Favorite() {
+    return <div className="favorites-empty">
+        <h2>No Favorite Movies Yet</h2>
+        <p></p>
+
+    </div>
+}
+export default Favorite
--- movie-app/src/pages/Home.jsx
+++ movie-app/src/pages/Home.jsx
@@ -5,17 +5,20 @@ import { useState } from "react";
 
 function Home(){
     const [searchQuery, setSearchQuery] = useState("");
+        //[name of the state, function to update the state]
 
 
 
     const movies =[
-        {id:1, title: 'Movie1', release_date: "2020"},
-        {id:2, title: 'Movie2', release_date: "2021"},
-        {id:3, title: 'Movie3', release_date: "2022"},
-        {id:4, title: 'Movie4', release_date: "2023"},
+        {id:1, title: 'The Night Agent', release_date: "2020"},
+        {id:2, title: 'Recruit', release_date: "2021"},
+        {id:3, title: 'Money Heist', release_date: "2022"},
+        {id:4, title: 'Stranger Things', release_date: "2023"},
     ];
       
-    const handleSearch = () => {
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
 
     }
 
@@ -23,14 +26,14 @@ function Home(){
     return (
     <div className="home">
         <form onSubmit={handleSearch} className="search-form">
-            <input type="text" placeholder="Search for movies..." className="search-input"></input>
+            <input type="text" placeholder="Search for movies" className="search-input" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}></input>
             <button type="submit" className="search-button">Search</button>
         </form>
 
 
         <div className="movies-grid">
             {movies.map((movie) => (
-                <MovieCard movie={movie} key={movie.id} />
+                movie.title.toLowerCase().startsWith(searchQuery) && <MovieCard movie={movie} key={movie.id} />
             ))}
         </div>
     </div>
